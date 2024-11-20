9426fede91eb6476db50abe521eba326e77036fa
0xMenna01 <emanuele.valzano@proton.me>
2024-11-20 03:17:25 +0100

    Data structures to handle feed messages.

--- src/batchposter.rs
+++ src/batchposter.rs
@@ -1,6 +1,13 @@
-use crate::types::{inbox_contract_from, BroadcastMessage, Config, InboxContract};
+use crate::types::{
+    inbox_contract_from, BatchPosterPosition, BroadcastFeedMessage, BroadcastMessage,
+    BuildingBatch, Config, InboxContract,
+};
 use anyhow::Result;
 use futures_util::StreamExt;
+use std::{
+    mem,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 use tokio::{
     sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
     task::JoinHandle,
@@ -8,19 +15,38 @@ use tokio::{
 };
 use tokio_tungstenite::connect_async;
 
+// This PoC implementation is not fault-tolerant:
+// It holds all the state in memory and does not have failure recovery mechanisms.
 pub struct BatchPoster {
     feed_handler: SequencerFeedHandler,
     sequencer_inbox: InboxContract,
     poll_interval: Interval,
+    max_batch_post_interval: Duration,
+    building_batch: Option<BuildingBatch>,
+    batchposter_position: BatchPosterPosition,
+    incoming_messages: Vec<BroadcastFeedMessage>,
+    first_incoming_msg_time: Option<SystemTime>,
+    msg_count: u64,
 }
 
 impl From<Config> for BatchPoster {
     fn from(config: Config) -> Self {
         let sequencer_inbox = inbox_contract_from(&config);
+
         Self {
+            feed_handler: SequencerFeedHandler::new(config.feed_url),
             sequencer_inbox,
             poll_interval: time::interval(config.poll_interval),
-            feed_handler: SequencerFeedHandler::new(config.feed_url),
+            max_batch_post_interval: config.max_batch_post_interval,
+            building_batch: Default::default(),
+            batchposter_position: BatchPosterPosition {
+                msg_count: Default::default(),
+                delayed_msg_count: Default::default(),
+                next_seq_number: Default::default(),
+            },
+            incoming_messages: Default::default(),
+            first_incoming_msg_time: Default::default(),
+            msg_count: Default::default(),
         }
     }
 }
@@ -38,8 +64,23 @@ impl BatchPoster {
         let consumer = self.feed_handler.consumer();
 
         while let Some(broadcast_msg) = consumer.recv().await {
-            println!("Received broadcast message: {:?}", broadcast_msg);
-            //self.poll_interval.tick().await;
+            println!("Received message: {:?}", broadcast_msg);
+            if !broadcast_msg.messages.is_empty() {
+                self.incoming_messages
+                    .extend_from_slice(&broadcast_msg.messages);
+
+                self.msg_count += broadcast_msg.messages.len() as u64;
+                println!("Total number of messages: {}", self.msg_count);
+            }
+
+            if self.first_incoming_msg_time.is_none() {
+                // find the first message to set the time of the batch initialization to later force a batch post
+                self.first_incoming_msg_time = self.incoming_messages.first().map(|msg| {
+                    UNIX_EPOCH + Duration::from_secs(msg.message.message.header.timestamp)
+                });
+            }
+
+            self.poll_interval.tick().await;
         }
 
         Ok(())
@@ -67,6 +108,29 @@ impl BatchPoster {
 
             Ok(())
         })
+    }
+
+    pub async fn maybe_post_batch(&mut self) -> Result<()> {
+        let batch_position = self.batchposter_position.clone();
+
+        if self.building_batch.is_none() {
+            self.building_batch = Some(BuildingBatch::new(
+                batch_position.msg_count,
+                batch_position.msg_count,
+                batch_position.delayed_msg_count,
+            ));
+        }
+
+        let force_post_batch = self.first_incoming_msg_time.map_or(false, |msg_time| {
+            msg_time.elapsed().unwrap() >= self.max_batch_post_interval
+        });
+
+        let dispatchable_messages = mem::take(&mut self.incoming_messages);
+        for msg in dispatchable_messages {
+            // process the message
+        }
+
+        Ok(())
     }
 }
 
--- src/types.rs
+++ src/types.rs
@@ -1,16 +1,18 @@
 use base64::engine::general_purpose::STANDARD as BASE64;
 use base64::Engine;
+use brotli::CompressorWriter;
 use ethers::{
     abi::Abi,
     contract::Contract,
     middleware::SignerMiddleware,
     providers::{Http, Provider},
     signers::LocalWallet,
-    types::{serde_helpers::deserialize_number, Address, H256, U256},
+    types::{serde_helpers::deserialize_number, Address, BlockId, BlockNumber, H256, U256},
 };
 use serde::Deserialize;
 use serde_json::Value;
 use std::{fs, sync::Arc, time::Duration};
+use tokio::time::{self, Interval};
 
 /// Simple configuration for the batch poster
 #[derive(Debug, Clone, Deserialize)]
@@ -21,6 +23,7 @@ pub struct Config {
     pub sequencer_inbox_address: Address,
     pub contract_abi_path: String,
     pub poll_interval: Duration,
+    pub max_batch_post_interval: Duration,
 }
 
 /// Default configuration
@@ -37,6 +40,7 @@ impl Default for Config {
                 .unwrap(),
             contract_abi_path: "SequencerInbox.json".to_string(),
             poll_interval: Duration::from_secs(12),
+            max_batch_post_interval: Duration::from_secs(300),
         }
     }
 }
@@ -56,6 +60,13 @@ pub fn inbox_contract_from(config: &Config) -> InboxContract {
     let sequencer_inbox = Contract::new(config.sequencer_inbox_address, abi, client);
 
     sequencer_inbox
+}
+
+#[derive(Debug, Clone)]
+pub struct BatchPosterPosition {
+    pub msg_count: u64,
+    pub delayed_msg_count: u64,
+    pub next_seq_number: u64,
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -178,15 +189,69 @@ impl<'de> Deserialize<'de> for Base64Bytes {
 
 pub struct BuildingBatch {
     /// Instance that handles the segments of the batch
-    segments: Vec<u8>, // TODO: Change to BatchSegments
+    segments: BatchSegments,
     /// Message count at the start of the batch.
     start_msg_count: u64,
-    /// Message count at the end of the batch.
+    /// Current message count of the batch.
     msg_count: u64,
-    /// Whether the batch has a useful message.
-    has_useful_message: bool,
-    /// The first non-delayed message in the batch.
-    first_non_delayed_msg: MessageWithMetadata,
-    /// The first useful message in the batch.
-    first_useful_msg: MessageWithMetadata,
+}
+
+impl BuildingBatch {
+    pub fn new(start_msg_count: u64, msg_count: u64, first_delayed: u64) -> Self {
+        let segments = BatchSegments::new(first_delayed);
+
+        Self {
+            segments,
+            start_msg_count,
+            msg_count,
+        }
+    }
+}
+
+pub struct BatchSegments {
+    compressed_buffer: Vec<u8>,
+    compressed_writer: CompressorWriter<Vec<u8>>,
+    raw_segments: Vec<Vec<u8>>,
+    timestamp: u64,
+    block_num: u64,
+    delayed_msg: u64,
+    size_limit: u32,
+    compression_level: u32,
+    new_uncompressed_size: u32,
+    total_uncompressed_size: u32,
+    last_compressed_size: u32,
+    trailing_headers: u32,
+    is_done: bool,
+}
+
+impl BatchSegments {
+    pub fn new(first_delayed: u64) -> Self {
+        let compressed_buffer = Vec::new();
+        let buffer_size = 4096;
+        let compression_level = 11;
+        let lgwin = 22;
+
+        let compressed_writer = CompressorWriter::new(
+            compressed_buffer.clone(),
+            buffer_size,
+            compression_level,
+            lgwin,
+        );
+
+        Self {
+            compressed_buffer,
+            compressed_writer,
+            size_limit: 0, // not actually used
+            compression_level,
+            raw_segments: Vec::new(),
+            delayed_msg: first_delayed,
+            timestamp: 0,
+            block_num: 0,
+            new_uncompressed_size: 0,
+            total_uncompressed_size: 0,
+            last_compressed_size: 0,
+            trailing_headers: 0,
+            is_done: false,
+        }
+    }
 }
